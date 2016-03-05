64bb42dbf7ae5ff831b08610d4eaa0f32a55de26
feibian <fabian_lerma@hotmail.com>
2016-03-05 16:36:46 -0700

    Added view and template for detailed post

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,3 +10,6 @@ def post_list(request):
     return render(request, 'blogapp/post_list.html', {'posts' : posts})
 
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogapp/post_detail.html', {'post': post})
